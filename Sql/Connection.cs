6fb0b33f822102ff36d6719648067b31f4e7790e
GấunoNgáo <bin81788@gmail.com>
2025-03-11 22:58:26 +0700

    11

--- Sql/Connection.cs
+++ Sql/Connection.cs
@@ -9,7 +9,7 @@ namespace Tra_Sua
 {
     class Connection
     {
-        public static string stringConnection = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""F:\C_C#_C++\Visual Studio Code\Tra_Sua\Sql\Tra_Sua.mdf"";Integrated Security=True";
+        public static string stringConnection = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\NITRO 5\Source\Repos\Tra_Sua\Sql\Tra_Sua.mdf"";Integrated Security=True";
         public static SqlConnection GetSqlConnection()
         {
             return new SqlConnection(stringConnection);
